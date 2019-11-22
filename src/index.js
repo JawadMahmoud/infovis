709ab9e5f35e7d99d8bb3465177965413938b89c
Jawad Mahmoud <jd.mahmoud@gmail.com>
2019-11-22 05:36:37 +0000

    Import amcharts and setup project structure including data

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "@amcharts/amcharts4/core";
+import "@amcharts/amcharts4/maps";
+import "@amcharts/amcharts4/charts";
+import "@amcharts/amcharts4-geodata/worldLow";
+import "@amcharts/amcharts4/themes/dataviz";
+import "@amcharts/amcharts4/themes/animated";
