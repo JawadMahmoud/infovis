a343eac453894b57c4023f42849503fa807f6504
2288115k <2288115k@student.gla.ac.uk>
2019-12-04 15:41:19 +0000

    using total deaths instead of consumption

--- py_data_formation/main.py
+++ py_data_formation/main.py
@@ -3,14 +3,15 @@ import csv
 c_list = []
 value_list= []
 consumption_c= []
-year = ' 2010'
+year = ' 2014'
+death_year = ' 2015'
 series = {'series': [{'name': 'Only government objectives on tobacco control exist', 'data' : []},
                      {'name': 'Only national agency for tobacco control exists', 'data' : []},
                      {'name': 'Both exist', 'data' : []},
                      {'name': 'Neither exist', 'data' : []}
                      ]}
 
-with open('../src/data/adult_consumption.csv') as csv_file:
+with open('../src/data/total_ncd_death.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -18,7 +19,7 @@ with open('../src/data/adult_consumption.csv') as csv_file:
             #print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            if row[5] != 'Not available':
+            if row[5] != 'Not available' and row[1] == death_year:
                 consumption_c.append(row[0])
                 value_list.append({'name':row[0],'value':float(row[5])})
 
